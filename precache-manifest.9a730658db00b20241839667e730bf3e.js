17857a80f161860d1b295a45c20cc409ce0d25e2
SpacyCoder <sral_h@hotmail.com>
2020-11-02 17:45:41 +0100

    Updates

---
+++ precache-manifest.9a730658db00b20241839667e730bf3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81edcdffaeafdb44799a9696d27ec2e2",
+    "url": "/norkart/index.html"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/norkart/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "fe0c7659973cd6e7cb8e",
+    "url": "/norkart/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/norkart/static/js/2.16ab8709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/norkart/static/js/2.16ab8709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe0c7659973cd6e7cb8e",
+    "url": "/norkart/static/js/main.23351a4f.chunk.js"
+  },
+  {
+    "revision": "f90a8507894a441cdbfe",
+    "url": "/norkart/static/js/runtime-main.67b1062b.js"
+  }
+]);
